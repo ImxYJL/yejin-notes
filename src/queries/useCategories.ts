21841e00fdbb5a9c47bfbbba12e1488187b2e693
ImxYJL <allensain14@gmail.com>
2026-02-28 20:46:54 +0900

    feat: useCategories에서 select 추가 - 카테고리 객체와 단순 배열 모두 리턴

--- src/queries/useCategories.ts
+++ src/queries/useCategories.ts
@@ -1,12 +1,23 @@
 import { getCategoriesApi } from "@/apis/category";
 import { useQuery } from "@tanstack/react-query";
 import { BLOG_QUERY_KEY } from "./queryKey";
+import { Category, CategoryMap } from "@/types/blog";
 
-export const useCategoryQuery = () => {
+const useCategories = () => {
   return useQuery({
     queryKey: [BLOG_QUERY_KEY.categories],
     queryFn: getCategoriesApi,
     staleTime: Infinity,
     gcTime: Infinity,
+    select: (categories: Category[]) => {
+      const categoryMap = categories.reduce((acc, category) => {
+        acc[category.slug] = category;
+        return acc;
+      }, {} as CategoryMap);
+
+      return { categories, categoryMap };
+    },
   });
 };
+
+export default useCategories;
