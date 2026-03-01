ab0c5b7737f71c240b05e71502e96c3d279cf965
ImxYJL <allensain14@gmail.com>
2026-03-01 16:44:55 +0900

    refactor: 전역 레이아웃에서 전역 API 데이터 호출

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,40 @@
 import "@/app/globals.css";
 import { Toast } from "@/components/common";
-
 import { pretendard } from "@/libs/fonts";
 import ClientProviders from "@/utils/providers/ClientProviders";
+import { makeQueryClient } from "@/libs/tanstack/queryClient";
+import { getAuthUser } from "@/services/authService";
+import { getCategories } from "@/services/categoryService";
+import { ADMIN_QUERY_KEY, BLOG_QUERY_KEY } from "@/queries/queryKey";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = makeQueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: [ADMIN_QUERY_KEY.authUser],
+      queryFn: getAuthUser,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: [BLOG_QUERY_KEY.categories],
+      queryFn: getCategories,
+    }),
+  ]);
+
   return (
     <html lang="ko" className={pretendard.variable}>
       <body>
         <Toast />
-        <ClientProviders>{children}</ClientProviders>
+        <ClientProviders>
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            {children}
+          </HydrationBoundary>
+        </ClientProviders>
       </body>
     </html>
   );
