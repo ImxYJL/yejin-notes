35c10b6d190218543cde910bd39645df64c93898
ImxYJL <allensain14@gmail.com>
2026-03-05 18:00:19 +0900

    chore: 이전 커밋에서 누락된 변경사항

--- src/app/(viewer)/[categorySlug]/posts/[id]/page.tsx
+++ src/app/(viewer)/[categorySlug]/posts/[id]/page.tsx
@@ -1,8 +1,6 @@
-import { PostDetail, PostDetailSkeleton } from "@/app/(viewer)/components";
-import { checkIsAdmin } from "@/services/authService";
-import { getPost } from "@/services/postService";
+import { PostDetailSkeleton } from "@/app/(viewer)/components";
+import PostDetailContainer from "@/app/(viewer)/components/PostDetailContainer";
 import { CategorySlug } from "@/types/blog";
-import { getMarkdownHtml } from "@/utils/markdown";
 import { Suspense } from "react";
 
 type PostDetailPageParams = {
@@ -17,20 +15,9 @@ const PostDetailPage = async ({
 }) => {
   const { categorySlug, id } = await params;
 
-  // TODO: await 분리
-  const post = await getPost(id);
-  const isAdmin = await checkIsAdmin();
-
-  const htmlContent = await getMarkdownHtml(post.content);
-
   return (
     <Suspense fallback={<PostDetailSkeleton />}>
-      <PostDetail
-        categorySlug={categorySlug}
-        post={post}
-        htmlContent={htmlContent}
-        isAdmin={isAdmin}
-      />
+      <PostDetailContainer categorySlug={categorySlug} id={id} />
     </Suspense>
   );
 };
--- src/app/(viewer)/components/PostDetail.tsx
+++ src/app/(viewer)/components/PostDetail.tsx
@@ -6,69 +6,65 @@ import MarkdownViewer from "@/components/markdown/MarkdownViewer";
 import { formatDate } from "@/utils/date";
 
 type Props = {
-  htmlContent: string;
+  contentNode: React.ReactNode;
   categorySlug: CategorySlug;
   post: PostDetailResponse;
   isAdmin: boolean;
 };
 
-const PostDetail = ({ categorySlug, htmlContent, isAdmin, post }: Props) => {
-  return (
-    <div className="space-y-16">
-      <article>
-        <header className="mb-4 space-y-2">
-          <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-2">
-            <h1 className="text-3xl font-extrabold tracking-tight leading-tight max-w-[80%]">
-              {post.title}
-            </h1>
+const PostDetail = ({ categorySlug, contentNode, isAdmin, post }: Props) => (
+  <div className="space-y-16">
+    <article>
+      <header className="mb-4 space-y-2">
+        <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-2">
+          <h1 className="text-3xl font-extrabold tracking-tight leading-tight max-w-[80%]">
+            {post.title}
+          </h1>
 
-            <div className="flex flex-col gap-2 items-end">
-              <span className="flex items-center gap-1.5 text-sm text-muted-foreground shrink-0 pb-1">
-                <Clock size={20} /> {formatDate(post.createdAt)}
-              </span>
-            </div>
-          </div>
-
-          <div className="flex justify-between items-center flex-wrap gap-4 text-sm text-muted-foreground">
-            <span className="flex items-center gap-1.5">
-              {post.isPrivate && (
-                <span className="text-palette-0 font-bold bg-palette-0/10 px-2 py-0.5 rounded flex items-center justify-center">
-                  <Lock size={20} />
-                </span>
-              )}
-              {post.tags.length > 0 && (
-                <>
-                  <Tag size={20} />
-                  {post.tags.map((tag, i) => (
-                    <span key={i}>{tag}</span>
-                  ))}
-                </>
-              )}
+          <div className="flex flex-col gap-2 items-end">
+            <span className="flex items-center gap-1.5 text-sm text-muted-foreground shrink-0 pb-1">
+              <Clock size={20} /> {formatDate(post.createdAt)}
             </span>
-
-            <div className="flex items-center">
-              {isAdmin && (
-                <PostAction id={post.id} categorySlug={categorySlug} />
-              )}
-            </div>
           </div>
-        </header>
+        </div>
 
-        <Divider direction="horizontal" className="mb-12" />
+        <div className="flex justify-between items-center flex-wrap gap-4 text-sm text-muted-foreground">
+          <span className="flex items-center gap-1.5">
+            {post.isPrivate && (
+              <span className="text-palette-0 font-bold bg-palette-0/10 px-2 py-0.5 rounded flex items-center justify-center">
+                <Lock size={20} />
+              </span>
+            )}
+            {post.tags.length > 0 && (
+              <>
+                <Tag size={20} />
+                {post.tags.map((tag, i) => (
+                  <span key={i}>{tag}</span>
+                ))}
+              </>
+            )}
+          </span>
 
-        <section className="bg-background rounded-main">
-          <MarkdownViewer htmlContent={htmlContent} />
-        </section>
-      </article>
+          <div className="flex items-center">
+            {isAdmin && <PostAction id={post.id} categorySlug={categorySlug} />}
+          </div>
+        </div>
+      </header>
 
-      <Divider direction="horizontal" className="opacity-80" />
+      <Divider direction="horizontal" className="mb-12" />
 
-      <PostNavigation
-        navigation={{ prevPost: post.prevPost, nextPost: post.nextPost }}
-        categorySlug={categorySlug}
-      />
-    </div>
-  );
-};
+      <section className="bg-background rounded-main">
+        <MarkdownViewer contentNode={contentNode} />
+      </section>
+    </article>
+
+    <Divider direction="horizontal" className="opacity-80" />
+
+    <PostNavigation
+      navigation={{ prevPost: post.prevPost, nextPost: post.nextPost }}
+      categorySlug={categorySlug}
+    />
+  </div>
+);
 
 export default PostDetail;
