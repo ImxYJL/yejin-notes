0427712f8e4afe3234aa09c2fd90fd589fa6afba
ImxYJL <allensain14@gmail.com>
2026-03-06 18:00:26 +0900

    refactor: 불필요한 prefetch query 제거

--- src/app/(viewer)/layout.tsx
+++ src/app/(viewer)/layout.tsx
@@ -1,25 +1,15 @@
 import { cn } from "@/utils/styles";
 import Sidebar from "@/components/common/Sidebar";
-import { makeQueryClient } from "@/libs/tanstack/queryClient";
 import { getAuthUser } from "@/services/authService";
 import { getCategories } from "@/services/categoryService";
-import { ADMIN_QUERY_KEY, BLOG_QUERY_KEY } from "@/queries/queryKey";
 
 const ViewerLayout = async ({ children }: { children: React.ReactNode }) => {
-  const queryClient = makeQueryClient();
-
   const [user, categories] = await Promise.all([
-    queryClient.fetchQuery({
-      queryKey: [ADMIN_QUERY_KEY.authUser],
-      queryFn: getAuthUser,
-    }),
-    queryClient.fetchQuery({
-      queryKey: [BLOG_QUERY_KEY.categories],
-      queryFn: getCategories,
-    }),
+    getAuthUser(),
+    getCategories(),
   ]);
 
-  const visibleCategories = user
+  const visibleCategories = user?.isAdmin
     ? categories
     : categories.filter((category) => !category.isPrivate);
 
