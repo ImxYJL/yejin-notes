b9cf56bb3eb0dbbf497828fb16671bbfd80ca55d
ImxYJL <allensain14@gmail.com>
2026-04-11 15:41:53 +0900

    fix: Edit 페이지에서 id 주입 제거

--- src/app/(admin)/admin/edit/[id]/page.tsx
+++ src/app/(admin)/admin/edit/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { PostForm } from '@/types/blog';
-import { INITIAL_POST } from '@/constants/blog';
 import { getAdminPost } from '@/services/postService';
 import { EditorContainer } from '../components';
 import { convertToPostForm } from '@/utils/posts';
+import { AppError } from '@/utils/error';
 
 type EditPageParams = {
   id: string;
@@ -10,15 +10,12 @@ type EditPageParams = {
 
 const EditPage = async ({ params }: { params: Promise<EditPageParams> }) => {
   const { id } = await params;
+  if (!id) AppError.notFound();
+
   const data = await getAdminPost(id).catch(() => null);
+  if (!data) AppError.notFound();
 
-  const initialPost: PostForm = {
-    ...INITIAL_POST,
-    id: crypto.randomUUID(),
-  };
-
-  const post: PostForm = data ? convertToPostForm(data) : initialPost;
-
+  const post: PostForm = convertToPostForm(data!);
   return <EditorContainer post={post} />;
 };
 
