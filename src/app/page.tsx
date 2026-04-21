aea421eeafd85e16e1e98357ef1582b8c20cfc8c
ImxYJL <allensain14@gmail.com>
2026-04-21 14:57:01 +0900

    chore: 루트 페이지에 사이드바 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-export default function Home() {
+import { cn } from '@/utils/styles';
+import Sidebar from '@/components/common/Sidebar';
+import { getPublicCategories } from '@/services/categoryService';
+import { makeQueryClient } from '@/libs/tanstack/queryClient';
+import { BLOG_QUERY_KEY } from '@/queries/queryKey';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { CATEGORY_FILTER } from '@/constants/blog';
+import { PAGE_PATH } from '@/constants/paths';
+
+export default async function Home() {
+  const queryClient = makeQueryClient();
+
+  const categories = await queryClient.fetchQuery({
+    queryKey: [BLOG_QUERY_KEY.categories, CATEGORY_FILTER.all],
+    queryFn: getPublicCategories,
+  });
+  const categoriesWithHref = categories.map((c) => ({
+    ...c,
+    href: PAGE_PATH.admin.posts(c.slug),
+  }));
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        루트에 뭘 넣을지 고민중
-      </main>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex min-h-screen bg-background">
+        <Sidebar categories={categoriesWithHref} />
+
+        <main
+          className={cn(
+            'flex-1 min-w-0 min-h-screen base-transition flex flex-col overflow-y-auto',
+            'mx-auto pb-20 pt-20',
+            'max-w-content w-full',
+            'px-6 md:px-16 lg:px-24 xl:px-32',
+          )}
+        ></main>
+      </div>
+    </HydrationBoundary>
   );
 }
