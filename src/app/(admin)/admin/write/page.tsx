4f83fc2d42702396d256b4448dda4bd8efe84ff4
ImxYJL <allensain14@gmail.com>
2026-05-03 17:12:40 +0900

    refactor: WritePage에도 변경사항 적용

--- src/app/(admin)/admin/write/page.tsx
+++ src/app/(admin)/admin/write/page.tsx
@@ -1,27 +1,21 @@
 import EditorForm from '@/components/editor/EditorForm';
 import { INITIAL_POST } from '@/constants/blog';
-import { makeQueryClient } from '@/libs/tanstack/queryClient';
-import { BLOG_QUERY_KEY } from '@/queries/queryKey';
 import { getAllCategories } from '@/services/categoryService';
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 // TODO: Write와 Edit 합치기
 const WritePage = async () => {
-  const queryClient = makeQueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: [BLOG_QUERY_KEY.categories],
-    queryFn: getAllCategories,
-  });
-
   const initialData = {
     ...INITIAL_POST,
     id: crypto.randomUUID(),
   };
 
+  const categories = await getAllCategories();
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <EditorForm initialData={initialData} mode="create" />
-    </HydrationBoundary>
+    <Suspense>
+      <EditorForm initialData={initialData} categories={categories} mode="create" />
+    </Suspense>
   );
 };
 
