bfd07e339f03a01d1cc2d962ec6887a019c596f4
ImxYJL <allensain14@gmail.com>
2026-07-11 18:20:36 +0900

    refactor: 불필요한 ref 참조 제거

--- src/components/editor/TitleInput.tsx
+++ src/components/editor/TitleInput.tsx
@@ -1,4 +1,4 @@
-import { memo, ChangeEvent } from 'react';
+import { ChangeEvent } from 'react';
 import { Input } from '../common';
 
 type TitleInputProps = {
@@ -6,19 +6,16 @@ type TitleInputProps = {
   onChange: (val: string) => void;
 };
 
-const TitleInput = memo(({ value, onChange }: TitleInputProps) => {
-  return (
-    <Input
-      name="title"
-      variant="ghost"
-      fontSize="3xl"
-      placeholder="제목을 입력하세요"
-      value={value}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
-      className="p-2 border-none font-bold placeholder:text-muted-foreground/40"
-    />
-  );
-});
+const TitleInput = ({ value, onChange }: TitleInputProps) => (
+  <Input
+    name="title"
+    variant="ghost"
+    fontSize="3xl"
+    placeholder="제목을 입력하세요"
+    value={value}
+    onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
+    className="p-2 border-none font-bold placeholder:text-muted-foreground/40"
+  />
+);
 
-TitleInput.displayName = 'TitleInput';
 export default TitleInput;
--- src/hooks/useAutoSave.ts
+++ src/hooks/useAutoSave.ts
@@ -3,16 +3,9 @@ import { PostForm } from '@/types/blog';
 import { debounce } from '@/utils/node';
 import { useEffect, useRef } from 'react';
 
+const AUTOSAVE_TIME = 5000;
+
 const useAutoSave = (formData: PostForm, setId: (id: string) => void) => {
-  // formData 클로저로 낡은 값 들어가는 것 방지용
-  const formDataRef = useRef(formData);
-
-  // 렌더 이후에 ref 업데이트를 예약 (렌더 중 ref 갱신할 경우 값이 모호하므로)
-  useEffect(() => {
-    formDataRef.current = formData;
-  }, [formData]);
-
-  const AUTOSAVE_TIME = 5000;
   const { mutate: saveDraft } = useSaveDraft(setId);
   const debouncedSave = useRef(
     debounce((data: PostForm) => saveDraft(data), AUTOSAVE_TIME),
@@ -24,7 +17,7 @@ const useAutoSave = (formData: PostForm, setId: (id: string) => void) => {
   }, []);
 
   const trigger = <K extends keyof PostForm>(field: K, value: PostForm[K]) => {
-    debouncedSave({ ...formDataRef.current, [field]: value });
+    debouncedSave({ ...formData, [field]: value });
   };
 
   // ref의 current값 평가 지연용
